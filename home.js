1257110c47e458e0b3f0b29b41638e4e07faf22b
kemigabocatherine <kemigabocatherine205@gmail.com>
2022-01-06 02:47:27 +0300

    Numbers in javascript

--- home.js
+++ home.js
@@ -10,3 +10,24 @@ console.log(b);
 var age = prompt('What is your age?');
 
 document.getElementById('someText').innerHTML = age;
+
+//Numbers in Javascript
+var num1 = 10;
+num1 = num1 + 1;
+console.log(num1);
+
+//increment
+var num2 = 15;
+num2++;
+console.log(num2);
+
+//decrement
+var num2 = 15;
+num2--;
+console.log(num2);
+
+num2 /= 3;
+console.log(num2);
+
+
+
