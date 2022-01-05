245f1d2016d55bf3358484514b70d6e54e378071
kemigabocatherine <kemigabocatherine205@gmail.com>
2022-01-06 01:51:08 +0300

    Add a javascript file and add javascript code to the new file

---
+++ home.js
@@ -0,0 +1,11 @@
+console.log('hello');
+console.log('hello there');
+//
+
+//Variables
+var b = 'smoothie';
+console.log(b);
+
+document.getElementById('someText').innerHTML = 'Hey there';
+
+prompt('What is your age?');
